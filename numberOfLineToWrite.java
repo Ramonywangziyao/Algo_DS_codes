a01e2d7cc69ffa1e5490c2830594c70a8e8acb54
Ziyao Wang <tonywangziyao@gmail.com>
2018-05-07 17:11:09 -0700

    numofline

--- numberOfLineToWrite.java
+++ numberOfLineToWrite.java
@@ -1,15 +1,10 @@
 class Solution {
     public int[] numberOfLines(int[] widths, String S) {
         int ct = 1, wid, lineSum = 0;
-        boolean one = false;
         for(char c : S.toCharArray()) {
             wid = widths[c-'a'];
-            if(lineSum+wid > 100) {
-                ct++;
-                lineSum = wid;
-            } else {
-                lineSum += wid;
-            }
+            if(lineSum+wid > 100) ct++;
+            lineSum = lineSum+wid > 100? wid : lineSum + wid;
         }
         return new int[]{ct,lineSum};
     }
