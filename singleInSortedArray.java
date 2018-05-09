29e503a517ac8ae2d3e00db8a8a6dc83f7f092b1
Ziyao Wang <tonywangziyao@gmail.com>
2018-05-09 09:44:59 -0700

    single

---
+++ singleInSortedArray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int len = nums.length, lo = 0, hi = len, mid = lo+(hi-lo)/2;
+        while((mid+1 < len && nums[mid+1] == nums[mid]) || (mid - 1 >= 0 && nums[mid-1] == nums[mid])) {
+            if(mid % 2 == 0) {
+                if(nums[mid-1] == nums[mid]) hi = mid;
+                else lo = mid;
+            } else {
+                if(nums[mid-1] == nums[mid]) lo = mid;
+                else hi = mid;
+            }
+            mid = lo+(hi-lo)/2;
+        }
+        return nums[mid];
+    }
+}
