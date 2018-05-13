49c9bc552f20684e74092817ec6f940235129eac
Ziyao Wang <tonywangziyao@gmail.com>
2018-05-13 14:23:34 -0700

    clibstairs

---
+++ clibingStairs.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 2) return n;
+        int[] mem = new int[n];
+        mem[0] = 1;
+        mem[1] = 2;
+        for(int i = 2; i < n; i++) mem[i] = mem[i-1]+mem[i-2];
+        return mem[n-1];
+    }
+}
