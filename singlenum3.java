57a4d3c27f54f8c9e3519771a51b1d6941dde10d
ziyao wang <tonywangziyao@gmail.com>
2018-06-12 19:19:00 -0700

    singlenum3

---
+++ singlenum3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int diff = 0;
+        for(int n : nums) {
+            diff ^= n;
+        }
+        diff &= -diff;
+        int[] ans = new int[2];
+        for(int n : nums) {
+            if((n & diff) == 0) {
+                ans[0] ^= n;
+            } else {
+                ans[1] ^= n;
+            }
+        }
+        return ans;
+    }
+}
