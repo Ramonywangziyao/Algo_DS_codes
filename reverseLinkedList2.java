7c9065958eb20ffeb93a3dc8a888d88f334adab0
ziyao wang <tonywangziyao@gmail.com>
2018-06-26 13:32:28 -0700

    reverselinkedlist2

---
+++ reverseLinkedList2.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (head == null) return null;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode cur = dummy.next;
+        for (int i = 1; i < m; i++) {
+            pre = pre.next;
+            cur = cur.next;
+        }
+        for (int i = 0; i < n - m; i++) {
+            ListNode tmp = cur.next;
+            cur.next = tmp.next;
+            tmp.next = pre.next;
+            pre.next = tmp;
+        }
+        return dummy.next;
+    }
+}
