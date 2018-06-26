9cf6c197327ba5e7cea2dbf98d0da4e05e06c8ed
ziyao wang <tonywangziyao@gmail.com>
2018-06-25 20:59:39 -0700

    searchrotates2

---
+++ searchInSortedRotatedArray.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] == target) return true;
+            if(nums[mid] > nums[left] || nums[mid] > nums[right]) {
+                if(target < nums[mid] && target >= nums[left]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if(nums[mid] < nums[right] || nums[mid] < nums[left]) {
+                if(target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                right--;
+            }
+        }
+        return false;
+    }
+}
