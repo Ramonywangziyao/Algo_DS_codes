a2785dd8a58e3146639610b430570d9f8160517e
Ziyao Wang <tonywangziyao@gmail.com>
2018-07-10 19:01:20 -0700

    maximumsquare

---
+++ maximumSquare.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximalSquare(char[][] a) {
+        if (a == null || a.length == 0 || a[0].length == 0) return 0;
+        int max = 0, m = a.length, n = a[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(a[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        return max * max;
+    }
+}
